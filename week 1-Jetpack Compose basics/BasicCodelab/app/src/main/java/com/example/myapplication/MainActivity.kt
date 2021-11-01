7b5330387e4584bbfda546e1f348200ba55ee1b9
sujin <rkdthd1234@naver.com>
2021-11-01 23:11:17 +0900

    Jetpack Compose basics - 4. Tweaking the UI

--- week 1-Jetpack Compose basics/BasicCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -26,7 +29,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    androidx.compose.material.Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
